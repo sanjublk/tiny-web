bce7c172b2bac050f7689a7d5ba5c1670dbd93f7
sanju <sanjusk08gmail.com>
2022-09-04 16:36:01 +0530

    Added basic routing functionalities with the ability to get request and send responses

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, net::TcpListener, io::{Read, Write}};
 
 use serde::Deserialize;
+use serde_json::Value;
+use url::Url;
 
 #[derive(Debug, Deserialize)]
 pub struct Request {
@@ -17,3 +19,103 @@ pub struct Response {
     pub status_code: i32,
     pub content_type: String,
 }
+
+pub struct TinyWeb {
+    route_table: HashMap<String, fn(Request) -> Response>,
+}
+
+impl TinyWeb {
+
+    pub fn new() -> Self {
+        Self {
+            route_table: HashMap::new()
+        }
+    }
+
+    pub fn run(&self, port: &str) {
+        let addr = format!("127.0.0.1:{}", port);
+        let listner = TcpListener::bind(addr).unwrap();
+        let mut buf = [0; 1024];
+        let mut n;
+        let mut req: Request;
+        let mut res: Response;
+        
+        for stream in listner.incoming() {
+            let mut res: Response = Response {
+                status_code:200,
+                body:format!("{{meh: {}}}", "ha"),
+                content_type:"application/json".to_owned()};
+            let mut stream = stream.unwrap();
+            req = {
+                n = stream.read(&mut buf).unwrap();
+                Self::parse_request(&buf[0..n])
+            };
+
+            buf.fill(0);
+            res = self.get_response(req);
+            stream.write_all(Self::build_response(res).as_bytes()).unwrap();
+        }
+
+    }
+
+    pub fn add_route(&mut self, route: &str, f: fn(Request) -> Response) {
+        self.route_table.insert(route.to_owned(), f);
+    }
+
+    fn get_response(&self, req: Request,) -> Response {
+        if self.route_table.contains_key(&req.path) {
+            self.route_table.get(&req.path).unwrap()(req)
+        }
+        
+        else {
+            Response {
+                status_code: 404,
+                body: format!("<h1>Not Found 404</h1>"),
+                content_type: "text/html".to_owned()
+            }
+        }
+    }
+
+    pub fn build_response(response: Response) -> String {
+        format!("HTTP/1.1 {}\r\nContent-Type: {}; charset=utf-8\r\nContent-Length: {}\r\nConnection: Closed\r\n\r\n{}", 
+        &response.status_code, &response.content_type, &response.body.len(), &response.body)
+    }
+
+    pub fn parse_request(buf: &[u8]) -> Request {
+        let request = String::from_utf8_lossy(buf).into_owned();
+        let request_lines: Vec<&str> = request.split("\r\n").collect();
+        let request_line: Vec<&str> = request_lines[0].split(" ").collect();
+        let method = request_line[0];
+        let mut body = String::new();
+        let mut headers: HashMap<String, String> = HashMap::new();
+        for (i, line) in request_lines[1..].iter().enumerate() {
+            if line == &"" {
+                let json: Result<Value, serde_json::Error> =
+                    serde_json::from_str(request_lines[i + 1]);
+                body = match json {
+                    Ok(v) => v.to_string(),
+                    Err(_) => request_lines[i + 1..request_lines.len()].join(""),
+                };
+                break;
+            }
+            let j: Vec<&str> = line.split(":").collect();
+            headers.insert(j[0].to_owned(), j[1].to_owned());
+        }
+        let parsed_url = Url::parse(&format!("https://dummybase.com{}", request_line[1])).unwrap();
+        let queries = parsed_url
+            .query_pairs()
+            .into_owned()
+            .collect::<HashMap<String, String>>();
+        let path = parsed_url.path().trim_end_matches("/").to_owned();
+
+        Request {
+            method: method.to_owned(),
+            headers: headers,
+            path: path,
+            queries: queries,
+            body: body,
+        }
+    }
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,15 @@
 mod lib;
-use std::{
-    collections::HashMap,
-    io::{Read, Write},
-    net::{TcpListener, TcpStream},
-};
 
-use serde_json::{json, Value};
-use tinyweb::Request;
-use url::Url;
 
-use lib::Response;
+fn index(req: lib::Request) -> lib::Response {
+    lib::Response { body: "<h1>welcome to home page</h1>".to_owned(), status_code: 200, content_type: "text/html".to_owned() }
+}
 
 fn main() {
-    let addr = "127.0.0.1:34254";
-    let listner = TcpListener::bind(addr).unwrap();
 
-    for stream in listner.incoming() {
-        let mut stream = stream.unwrap();
-        let res = handle_connection(&mut stream);
-        stream.write_all(b"meh");
-    }
+    let mut web = lib::TinyWeb::new();
+    web.add_route("", index);
+    web.run("1234");
+
 }
 
-pub fn handle_connection(stream: &mut TcpStream) -> Response{
-    let mut buffer = [0; 1024];
-    let byte_n = stream.read(&mut buffer).unwrap();
-    let request = parse(&buffer[0..byte_n]);
-    dbg!(request);
-    Response {
-        body: "Meh, This is my response".to_owned(),
-        content_type: "type".to_owned(),
-        status_code: 200
-    }
-}
-
-pub fn parse(buf: &[u8]) -> Request {
-    let request = String::from_utf8_lossy(buf).into_owned();
-    let request_lines: Vec<&str> = request.split("\r\n").collect();
-    let request_line: Vec<&str> = request_lines[0].split(" ").collect();
-    let method = request_line[0];
-    dbg!(&request_lines);
-    let mut body = String::new();
-    let mut headers: HashMap<String, String> = HashMap::new();
-    for (i, line) in request_lines[1..].iter().enumerate() {
-        if line == &"" {
-            let json: Result<Value, serde_json::Error> = serde_json::from_str(request_lines[i + 1]);
-            body = match json {
-                Ok(v) => v.to_string(),
-                Err(_) => request_lines[i + 1..request_lines.len()].join(""),
-            };
-            break;
-        }
-        let j: Vec<&str> = line.split(":").collect();
-        headers.insert(j[0].to_owned(), j[1].to_owned());
-    }
-    let parsed_url = Url::parse(&format!("https://dummybase.com{}", request_line[1])).unwrap();
-    let queries = parsed_url
-        .query_pairs()
-        .into_owned()
-        .collect::<HashMap<String, String>>();
-    let path = parsed_url.path().to_owned();
-
-    Request {
-        method: method.to_owned(),
-        headers: headers,
-        path: path,
-        queries: queries,
-        body: body,
-    }
-}
